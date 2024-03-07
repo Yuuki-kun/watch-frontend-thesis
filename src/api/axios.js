656bd593fdd83b472f95da0bac31cba1574288af
Yuuki-kun <tongcongminhart@gmail.com>
2024-03-07 19:08:30 +0700

    update be domain

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.30:8080";
+// const BASE_URL = "http://192.168.1.30:8080";
+const BASE_URL = "https://watch-be-production.up.railway.app";
 
 export default axios.create({
   baseURL: BASE_URL,
