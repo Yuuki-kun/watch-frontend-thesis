4573539efd95d20ebb4d562d98cc8f2737ecd996
Yuuki-kun <tongcongminhart@gmail.com>
2024-03-08 17:11:36 +0700

    re server base url

--- src/api/axiosPrivate.js
+++ src/api/axiosPrivate.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "http://192.168.1.30:8080";
-const BASE_URL = "https://watch-be-production.up.railway.app";
+const BASE_URL = "http://192.168.1.30:8080";
+// const BASE_URL = "https://watch-be-production.up.railway.app";
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
